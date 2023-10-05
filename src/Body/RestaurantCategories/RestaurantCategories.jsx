8121d7978d2842c84b15c92649b233bc0051338c
itisameerkhan <itisameerkhan@gmail.com>
2023-10-05 09:21:27 +0530

    05/10/2023 09:21

--- src/Body/RestaurantCategories/RestaurantCategories.jsx
+++ src/Body/RestaurantCategories/RestaurantCategories.jsx
@@ -4,18 +4,15 @@ import { useState } from 'react';
 
 const RestaurantCategories = ({resCategory, showItems, setShowIndex}) => {
 
-    const [chevronState, setChevronState] = useState("fa-solid fa-chevron-down");
-
     const HandleClick = () => {
         setShowIndex()
-        setChevronState(chevronState === 'fa-solid fa-chevron-down' ? 'fa-solid fa-chevron-up' : 'fa-solid fa-chevron-down');
     }
 
     return (
         <div className="res-category">
             <div className="res-category-accordion-button" onClick={HandleClick}>
             <h3>{resCategory?.title} ({resCategory?.itemCards.length})</h3>
-            <i className={chevronState}></i>
+            <i className='fa-solid fa-chevron-down'></i>
             </div>
             {showItems && <AccordionMenu data={resCategory?.itemCards} />}
             <div className="bottom-div"></div>
